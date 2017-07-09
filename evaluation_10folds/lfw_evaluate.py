add lfw_evaluate() without k-folds, this evaluation has the same output as the matlab code; fix normalization bugs in load_features.py; rename calc_roc.py in to roc.py;

evaluation_10folds/lfw_evaluate.py
@@ -29,24 +29,92 @@ from __future__ import print_function
 
 import os
 import numpy as np
-import calc_roc
+import roc
 
 
-def evaluate(embeddings, actual_issame, nrof_folds=10):
-    # Calculate evaluation metrics
-    thresholds = np.arange(0, 4, 0.01)
-    embeddings1 = embeddings[0]
-    embeddings2 = embeddings[1]
-    print('embeddings1.shape: {}'.format(embeddings1.shape))
-    print('embeddings2.shape: {}'.format(embeddings2.shape))
+def calc_distance(features1, features2, distance='cosine'):
+    print('calc_distance: using {} distance'.format(distance))
 
-    tpr, fpr, accuracy = calc_roc.calculate_roc(thresholds, embeddings1, embeddings2,
-                                                np.asarray(actual_issame),
-                                                nrof_folds=nrof_folds)
-    thresholds = np.arange(0, 4, 0.001)
-    val, val_std, far = calc_roc.calculate_val(thresholds, embeddings1, embeddings2,
-                                               np.asarray(actual_issame), 1e-3, 
-                                               nrof_folds=nrof_folds)
+    # consinde distance = 1 - (emb1/norm(emb1))*(emb2/norm(emb2)).T
+    if distance == 'cosine':
+        tmp = features1 * features2
+        dist = 1 - np.sum(tmp, 1)
+    else:  # default 'squared' distance=diff^2
+        diff = np.subtract(features1, features2)
+        dist = np.sum(np.square(diff), 1)
+
+#    print 'dist.shape: ', dist.shape
+
+    return dist
+
+
+def lfw_evaluate(features, actual_issame, distance):
+    # calc evaluation metrics
+    #    print('Using: {} distance'.format(distance))
+
+    if distance == 'cosine':
+        max_thresh = 1.0
+        thresh_step = 0.00025
+    else:
+        max_thresh = 4.0
+        thresh_step = 0.001
+
+#    thresholds = np.arange(0, max_thresh, thresh_step)
+    thresholds = None
+
+    features1 = features[0]
+    features2 = features[1]
+
+#    print('features1.shape: {}'.format(features1.shape))
+#    print('features2.shape: {}'.format(features2.shape))
+
+    dist = calc_distance(features1, features2, distance)
+
+    tpr, fpr, best_accuracy = roc.calc_roc(thresholds,
+                                           dist,
+                                           actual_issame)
+
+#    thresholds = np.arange(0, max_thresh, thresh_step)
+
+    val_far = roc.calc_val(thresholds,
+                           dist,
+                           actual_issame
+                           )
+    return tpr, fpr, best_accuracy, val_far
+
+
+def lfw_evaluate_kfolds(features, actual_issame, distance, nrof_folds=10):
+    # calc evaluation metrics
+    #    print('Using: {} distance'.format(distance))
+
+    if distance == 'cosine':
+        max_thresh = 1.0
+        thresh_step = 0.00025
+    else:
+        max_thresh = 4.0
+        thresh_step = 0.001
+
+#    thresholds = np.arange(0, max_thresh, thresh_step)
+    thresholds = None
+
+    features1 = features[0]
+    features2 = features[1]
+
+#    print('features1.shape: {}'.format(features1.shape))
+#    print('features2.shape: {}'.format(features2.shape))
+#
+    dist = calc_distance(features1, features2, distance)
+
+    tpr, fpr, accuracy = roc.calc_roc_kfolds(thresholds,
+                                             dist,
+                                             actual_issame)
+
+#    thresholds = np.arange(0, max_thresh, thresh_step)
+
+    val, val_std, far = roc.calc_val_kfolds(thresholds,
+                                            dist,
+                                            actual_issame,
+                                            1e-3)
     return tpr, fpr, accuracy, val, val_std, far
 
 

evaluation_10folds/load_features.py
@@ -5,8 +5,9 @@ from scipy.io import loadmat
 from scipy.linalg import norm
 
 
-def load_mat_features(data_mat_file, pairs_mat_file, nrof_kfolds=10, nrof_features_per_fold=600):
-
+def load_mat_features(data_mat_file, pairs_mat_file,
+                      nrof_kfolds=10,
+                      nrof_features_per_fold=600):
     nrof_features_per_fold /= 2
 #    print nrof_features_per_fold
 
@@ -15,8 +16,8 @@ def load_mat_features(data_mat_file, pairs_mat_file, nrof_kfolds=10, nrof_featur
 
     pos_pairs = pairs['pos_pair'] - 1
     neg_pairs = pairs['neg_pair'] - 1
-    print "pos_pairs.shape: ", pos_pairs.shape
-    print "neg_pairs.shape: ", neg_pairs.shape
+#    print "pos_pairs.shape: ", pos_pairs.shape
+#    print "neg_pairs.shape: ", neg_pairs.shape
 
     assert(pos_pairs.shape == neg_pairs.shape)
     assert(pos_pairs.shape[0] == 2)
@@ -25,7 +26,7 @@ def load_mat_features(data_mat_file, pairs_mat_file, nrof_kfolds=10, nrof_featur
     assert(neg_pairs.shape[1] == nrof_features_per_fold * nrof_kfolds)
 
     features = data['features']
-    print "features.shape: ", features.shape
+#    print "features.shape: ", features.shape
 
     pos_ind_1 = pos_pairs[0].reshape(nrof_kfolds, nrof_features_per_fold)
     pos_ind_2 = pos_pairs[1].reshape(nrof_kfolds, nrof_features_per_fold)
@@ -47,20 +48,27 @@ def load_mat_features(data_mat_file, pairs_mat_file, nrof_kfolds=10, nrof_featur
 #    print ind_1.shape
 #    print ind_2.shape
 
-    embeddings1 = features[ind_1]
-    embeddings2 = features[ind_2]
+    ftrs1 = features[ind_1]
+    ftrs2 = features[ind_2]
 
-    emd_norm = norm(embeddings1, axis=0)
-    embeddings1 = embeddings1 / emd_norm
+#    print ftrs1.shape
+#    print ftrs2.shape
 
-    emd_norm = norm(embeddings2, axis=0)
-    embeddings2 = embeddings2 / emd_norm
+    ftr_norm = norm(ftrs1, axis=1)
+    ftr_norm = ftr_norm.reshape((-1,1))
+    ftrs1 = ftrs1 / ftr_norm
 
-#    print embeddings1.shape
-#    print embeddings1.shape
+    ftr_norm = norm(ftrs2, axis=1)
+    ftr_norm = ftr_norm.reshape((-1,1))
+    ftrs2 = ftrs2 / ftr_norm
 
-    embeddings = np.concatenate(
-        (embeddings1[np.newaxis, ...], embeddings2[np.newaxis, ...])
+#    print ftrs1.shape
+#    print ftrs1.shape
+#    print norm(ftrs1, axis=0)
+#    print norm(ftrs2, axis=0)
+
+    ftrs = np.concatenate(
+        (ftrs1[np.newaxis, ...], ftrs2[np.newaxis, ...])
     )
 
 #     tmp = np.hstack((np.ones(nrof_features_per_fold),
@@ -71,17 +79,17 @@ def load_mat_features(data_mat_file, pairs_mat_file, nrof_kfolds=10, nrof_featur
                  nrof_features_per_fold) * nrof_kfolds
     gt_labels = np.array(gt_labels)
 
-    return (embeddings, gt_labels)
+    return (ftrs, gt_labels)
 
 
 if __name__ == "__main__":
     data_mat_file = r'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/face_snapshot_0509_val0.1_batch476_iter_36000_fixbug.mat'
     pairs_mat_file = './lfw_pairs_zyf.mat'
-    (embeddings, gt_labels) = load_mat_features(data_mat_file,
-                                                pairs_mat_file,
-                                                nrof_kfolds=10,
-                                                nrof_features_per_fold=600)
+    (ftrs, gt_labels) = load_mat_features(data_mat_file,
+                                          pairs_mat_file,
+                                          nrof_kfolds=10,
+                                          nrof_features_per_fold=600)
 
-    print('embeddings.shape: {}'.format(embeddings.shape))
+    print('ftrs.shape: {}'.format(ftrs.shape))
     print('gt_labels.shape: {}'.format(gt_labels.shape))
 #    print('len(gt_labels): {}'.format(len(gt_labels)))

evaluation_10folds/validate_on_lfw.py
@@ -10,8 +10,26 @@ from sklearn import metrics
 from scipy.optimize import brentq
 from scipy import interpolate
 
-import lfw
 from load_features import load_mat_features
+from lfw_evaluate import lfw_evaluate, lfw_evaluate_kfolds
+
+import matplotlib.pyplot as plt
+
+
+def get_auc(fpr, tpr):
+    auc = metrics.auc(fpr, tpr)
+
+    return auc
+
+
+def get_eer(fpr, tpr):
+    x_min = fpr.min()
+    x_max = fpr.max()
+
+    f = interpolate.interp1d(fpr, tpr)
+    eer = brentq(lambda x: 1. - x - f(x), x_min, x_max)
+
+    return eer
 
 
 def main(args):
@@ -19,26 +37,65 @@ def main(args):
 #    for k,v in args:
 #        print "{}: {}".format(k,v)
 
-    emb_array, actual_issame = load_mat_features(args.feature_mat_file,
+    ftr_mat, actual_issame = load_mat_features(args.feature_mat_file,
                                                  args.lfw_pairs_mat_file,
                                                  args.lfw_nrof_folds,
                                                  args.nrof_features_per_fold)
 
-    print('emb_array.shape: {}'.format(emb_array.shape))
+    print('ftr_mat.shape: {}'.format(ftr_mat.shape))
     print('actual_issame.shape: {}'.format(actual_issame.shape))
-#    print('len(actual_issame): {}'.format(len(actual_issame)))
 
-    tpr, fpr, accuracy, val, val_std, far = lfw.evaluate(emb_array,
-                                                         actual_issame, 
-                                                         nrof_folds = args.lfw_nrof_folds)
+    print('\n=======================================')
+    print('EVALUATION WITHOUT K-FOLDS')
 
-    print('Accuracy: %1.3f+-%1.3f' % (np.mean(accuracy), np.std(accuracy)))
+    tpr, fpr, best_accuracy, val_far = lfw_evaluate(ftr_mat,
+                                                    actual_issame,
+                                                    distance=args.distance)
+
+    if args.draw_roc:
+        plt.figure()
+        plt.plot(fpr, tpr)
+        plt.title('ROC without K-folds')
+        plt.show()
+
+    print('Accuracy: %2.5f @distance_threshold=%2.5f, VAL=%2.5f, FAR=%2.5f'
+          % (best_accuracy['accuracy'],
+             best_accuracy['threshold'],
+             best_accuracy['VAL'],
+             best_accuracy['FAR']
+             )
+          )
+
+    auc = get_auc(fpr, tpr)
+    print('Area Under Curve (AUC): %2.5f' % auc)
+
+    eer = get_eer(fpr, tpr)
+    print('Equal Error Rate (EER): %2.5f with accuracy=%2.5f' % (eer, 1.0-eer))
+
+    for it in val_far:
+        print('Validation rate: %2.5f @ FAR=%2.5f with theshold %2.5f' %
+              (it['VAL'], it['FAR'], it['threshold']))
+
+    print('\n=======================================')
+    print('EVALUATION WITH K-FOLDS')
+    tpr, fpr, accuracy, val, val_std, far = lfw_evaluate_kfolds(ftr_mat,
+                                                                actual_issame,
+                                                                distance=args.distance,
+                                                                nrof_folds=args.lfw_nrof_folds)
+    if args.draw_roc:
+        plt.figure()
+        plt.plot(fpr, tpr)
+        plt.title('ROC with K-folds')
+        plt.show()
+
+    print('Accuracy: %2.5f+-%2.5f' % (np.mean(accuracy), np.std(accuracy)))
     print('Validation rate: %2.5f+-%2.5f @ FAR=%2.5f' % (val, val_std, far))
 
-    auc = metrics.auc(fpr, tpr)
-    print('Area Under Curve (AUC): %1.3f' % auc)
-    eer = brentq(lambda x: 1. - x - interpolate.interp1d(fpr, tpr)(x), 0., 1.)
-    print('Equal Error Rate (EER): %1.3f' % eer)
+    auc = get_auc(fpr, tpr)
+    print('Area Under Curve (AUC): %2.5f' % auc)
+
+    eer = get_eer(fpr, tpr)
+    print('Equal Error Rate (EER): %2.5f with accuracy=%2.5f' % (eer, 1.0-eer))
 
 
 def parse_arguments(argv):
@@ -46,14 +103,44 @@ def parse_arguments(argv):
 
     parser.add_argument('feature_mat_file', type=str,
                         help='Path to the .mat feature file.')
+    parser.add_argument('--distance', type=str,
+                        help='how to calc distance: <cosine or squared>.',
+                        default='cosine')
     parser.add_argument('--lfw_pairs_mat_file', type=str,
-                        help='The file containing the pairs to use for validation.', default='./lfw_pairs_zyf.mat')
+                        help='The file containing the pairs to use for validation.',
+                        default='./lfw_pairs_zyf.mat')
     parser.add_argument('--lfw_nrof_folds', type=int,
-                        help='Number of folds to use for cross validation. Mainly used for testing.', default=10)
+                        help='Number of folds to use for cross validation. Mainly used for testing.',
+                        default=10)
     parser.add_argument('--nrof_features_per_fold', type=int,
-                        help='Number of pairs in each fold.', default=600)
+                        help='Number of pairs in each fold.',
+                        default=600)
+    parser.add_argument('--draw_roc', type=int,
+                        help='draw ROC using matplot or not.',
+                        default=0)
     return parser.parse_args(argv)
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        ftr_path = r'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/center_face_model_fixbug.mat'
+#        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/face_snapshot_0504_val0.15_iter_28000_fixbug.mat'
+#        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/face_snapshot_0505_val0.1_iter_28000_fixbug.mat'
+#        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/face_snapshot_0505_val0.1_iter_50000_fixbug.mat'
+#        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/face_snapshot_0507_val0.1_batch416_iter_50000_fixbug.mat'
+#        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/face_snapshot_0509_val0.1_batch476_iter_36000_fixbug.mat'
+
+#        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/lfw-mtcnn-aligned-224x224_vgg-face_ftr.mat'
+#        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/lfw-ftr-nowarp-224x224_vgg-face.mat'
+#
+#        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/LFW-mtcnn-aligned-96x112_center_face_model_orig.mat'
+#        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/LFW-mtcnn-aligned-96x112_0504_val0.15_iter_28000.mat'
+
+        distance = 'cosine'
+        draw_roc = 0
+
+        sys.argv.append(ftr_path)
+        sys.argv.append('--distance=' + distance)
+        sys.argv.append('--draw_roc={}'.format(draw_roc))
+
     main(parse_arguments(sys.argv[1:]))