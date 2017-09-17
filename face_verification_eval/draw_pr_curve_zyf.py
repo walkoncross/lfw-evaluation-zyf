d831eb57c727108a34bd6fa6484ae4a06c770224
zyf <zhaoyafei0210@gmail.com>
2017-09-18 07:06:45 +0800

    add face_verification_eval

---
+++ face_verification_eval/draw_pr_curve_zyf.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import numpy as np
+import matplotlib.pyplot as plt
+import os.path as osp
+import os
+
+default_num_threshs = 2000
+default_threshs = np.linspace(0, 1, default_num_threshs, endpoint=False)
+
+fname_pr = "pr_curve_%d_threshs.txt" % default_num_threshs
+fname_roc = "roc_curve_%d_threshs.txt" % default_num_threshs
+fname_balanced_pr = "balanced_pr_curve_%d_threshs.txt" % default_num_threshs
+
+fname_pr_img = 'pr_curve_%d_threshs.png' % default_num_threshs
+fname_roc_img = "roc_curve_%d_threshs.png" % default_num_threshs
+fname_balanced_pr_img = 'balanced_pr_curve_%d_threshs.png' % default_num_threshs
+
+
+def load_roc_txt(fname_roc):
+    if not osp.exists(fname_roc):
+        return None
+
+    print "Load ROC from file " + fname_roc
+    fp_roc = open(fname_roc, 'r')
+    line = fp_roc.readline()  # skip the 1st line
+
+    tp = []
+    fn = []
+    tn = []
+    fp = []
+
+    for line in fp_roc:
+        spl = line.split()
+        tp.append(float(spl[1]))
+        fn.append(float(spl[2]))
+        tn.append(float(spl[3]))
+        fp.append(float(spl[4]))
+
+    tp = np.array(tp)
+    fn = np.array(fn)
+    tn = np.array(tn)
+    fp = np.array(fp)
+
+    fp_roc.close()
+
+    return tp, fn, tn, fp
+
+
+def load_similarity_file(sim_file):
+    sim_list = []
+    with open(sim_file, 'r') as fp_sim:
+        for line in fp_sim:
+            img1_fn, img2_fn, score = line.split('\t')
+            sim_list.append(float(score))
+    fp_sim.close()
+
+    return sim_list
+
+
+def calc_roc(same_pairs_sim_list, diff_pairs_sim_list, threshs=None, save_dir='./'):
+    if threshs is None:
+        threshs = default_threshs
+
+    if not osp.exists(save_dir):
+        os.makedirs(save_dir)
+
+    num_threshs = len(threshs)
+
+    fn = np.zeros(num_threshs)
+    tp = np.zeros(num_threshs)
+    tn = np.zeros(num_threshs)
+    fp = np.zeros(num_threshs)
+
+    print "Processing same pairs"
+    for score in same_pairs_sim_list:
+        for i in range(num_threshs):
+            if score < threshs[i]:
+                fn[i] += 1
+            else:
+                tp[i] += 1
+
+    print "Finished Processing same pairs"
+
+    print "Processing diff paris"
+    for score in diff_pairs_sim_list:
+        for i in range(num_threshs):
+            if score < threshs[i]:
+                tn[i] += 1
+            else:
+                fp[i] += 1
+    print "Finished Processing diff paris"
+
+    recall = tp / (tp + fn)
+    fpr = fp / (tn + fp)
+    fp_roc = open(osp.join(save_dir, fname_roc), "w")
+
+    fp_roc.write("thresh\t tp\t fn\t tn\t fp\t tpr\t \tfpr\n")
+    for i in range(num_threshs):
+        fp_roc.write("%f\t %d\t %d\t %d\t %d\t %f\t %f\n" %
+                     (threshs[i], tp[i], fn[i], tn[i], fp[i], recall[i], fpr[i]))
+
+    fp_roc.close()
+
+    return tp, fn, tn, fp
+
+
+def load_file_and_calc_roc(same_pairs_result_file, diff_pairs_result_file,
+                           threshs=None, save_dir='./'):
+    if threshs is None:
+        threshs = default_threshs
+
+    if not osp.exists(save_dir):
+        os.makedirs(save_dir)
+
+    same_pairs_sim_list = load_similarity_file(same_pairs_result_file)
+    diff_pairs_sim_list = load_similarity_file(diff_pairs_result_file)
+
+    tp, fn, tn, fp = calc_roc(
+        same_pairs_sim_list, diff_pairs_sim_list, threshs, save_dir)
+
+    return tp, fn, tn, fp
+
+
+def calc_presicion_recall(tp, fn, tn, fp, threshs=None, save_dir='./'):
+    if threshs is None:
+        threshs = default_threshs
+
+    if not osp.exists(save_dir):
+        os.makedirs(save_dir)
+
+    num_threshs = len(threshs)
+
+    recall = tp / (tp + fn)
+    precision = tp / (tp + fp)
+
+    fp_pr = open(osp.join(save_dir, fname_pr), "w")
+
+    fp_pr.write("thresh\t presicion\t recall\n")
+    for i in range(num_threshs):
+        fp_pr.write("%f\t %f\t %f\n" %
+                    (threshs[i], precision[i], recall[i]))
+
+    fp_pr.close()
+
+    balanced_precision = tp / (tp + fp / 3.1)
+
+    fp_pr_bl = open(osp.join(save_dir, fname_balanced_pr), "w")
+
+    fp_pr_bl.write("thresh\t presicion\t recall\n")
+    for i in range(num_threshs):
+        fp_pr_bl.write("%f\t %f\t %f\n" %
+                       (threshs[i], balanced_precision[i], recall[i]))
+
+    fp_pr_bl.close()
+
+
+# def calculate_pr(same_pairs_result_file, diff_pairs_result_file):
+#     print "Calc PR with %d threshs" % num_threshs
+
+#     fn = np.zeros(num_threshs)
+#     tp = np.zeros(num_threshs)
+#     tn = np.zeros(num_threshs)
+#     fp = np.zeros(num_threshs)
+
+#     print "Processing same pairs"
+#     with open(same_pairs_result_file, 'r') as fp_sim:
+#         for line in fp_sim:
+#             img1_fn, img2_fn, score = line.split('\t')
+#             score = float(score)
+
+#             for i in range(num_threshs):
+#                 if score < threshs[i]:
+#                     fn[i] += 1
+#                 else:
+#                     tp[i] += 1
+
+#     print "Finished Processing same pairs"
+
+#     print "Processing diff paris"
+#     with open(diff_pairs_result_file, 'r') as fp_sim:
+#         for line in fp_sim:
+#             img1_fn, img2_fn, score = line.split('\t')
+#             score = float(score)
+
+#             for i in range(num_threshs):
+#                 if score < threshs[i]:
+#                     tn[i] += 1
+#                 else:
+#                     fp[i] += 1
+#     print "Finished Processing diff paris"
+
+#     recall = tp / (tp + fn)
+#     precision = tp / (tp + fp)
+#     fpr = fp / (tn + fp)
+
+#     fp_pr = open(fname_pr, "w")
+#     fp_roc = open(fname_roc, "w")
+
+#     fp_pr.write("thresh\t presicion\t recall\n")
+#     fp_roc.write("thresh\t tp\t fn\t tn\t fp\t tpr\t \tfpr\n")
+#     for i in range(num_threshs):
+#         fp_pr.write("%f\t %f\t %f\n" %
+#                     (threshs[i], precision[i], recall[i]))
+#         fp_roc.write("%f\t %d\t %d\t %d\t %d\t %f\t %f\n" %
+#                      (threshs[i], tp[i], fn[i], tn[i], fp[i], recall[i], fpr[i]))
+
+#     fp_pr.close()
+#     fp_roc.close()
+
+#     balanced_precision = tp / (tp + fp / 3.1)
+
+#     fp_pr_bl = open(fname_balanced_pr, "w")
+
+#     fp_pr_bl.write("thresh\t presicion\t recall\n")
+#     for i in range(len(threshs=None)):
+#         fp_pr_bl.write("%f\t %f\t %f\n" %
+#                        (threshs[i], balanced_precision[i], recall[i]))
+
+#     fp_pr_bl.close()
+
+#     return tp, fn, tn, fp
+
+
+def calc_roc_and_pr(same_pairs_result_file, diff_pairs_result_file, threshs=None, save_dir='./'):
+    if threshs is None:
+        threshs = default_threshs
+
+    tp, fn, tn, fp = load_file_and_calc_roc(
+        same_pairs_result_file, diff_pairs_result_file, threshs, save_dir)
+
+    calc_presicion_recall(tp, fn, tn, fp, threshs, save_dir)
+
+    return tp, fn, tn, fp
+
+
+def draw_analysis_figure(tp, fn, tn, fp, save_dir='./'):
+    if not osp.exists(save_dir):
+        os.makedirs(save_dir)
+
+    recall = tp / (tp + fn)
+    precision = tp / (tp + fp)
+
+    print "Draw PR curve"
+    plt.figure(1)
+    plt.plot(recall, precision)
+
+    plt.xticks(np.arange(0, 1.1, 0.1))
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.title('Precision vs. Recall Curve')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.grid(color='r', linestyle='--', linewidth=1)
+    plt.show()
+    plt.savefig(osp.join(save_dir, fname_pr_img))
+
+    fpr = fp / (fp + tn)
+
+    print "Draw ROC curve"
+    plt.figure(2)
+    plt.plot(fpr, recall)
+
+    plt.xticks(np.arange(0, 1.1, 0.1))
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.title('TPR(recall) vs. FPR Curve')
+    plt.xlabel('FPR')
+    plt.ylabel('Recall')
+    plt.grid(which='both', color='r', linestyle='--', linewidth=1)
+    plt.show()
+    plt.savefig(osp.join(save_dir, fname_roc_img))
+
+    balanced_precision = tp / (tp + fp / 3.1)
+
+#    fp_pr = open(fname_balanced_pr, "w")
+#
+#    fp_pr.write("thresh\t presicion\t recall\n")
+#    for i in range(num_threshs):
+#        fp_pr.write("%f\t %f\t %f\n" % (threshs[i], balanced_precision[i], recall[i]))
+#
+#    fp_pr.close()
+
+#    fp_roc = open(fname_roc, "w")
+#
+#    fp_roc.write("thresh\t tp\t fn\t tn\t fp\t tpr\t \tfpr\n")
+#    for i in range(num_threshs):
+#        fp_roc.write("%f\t %d\t %d\t %d\t %d\t %f\t %f\n" % (threshs[i], tp[i], fn[i], tn[i], fp[i], recall[i], fpr[i]))
+#
+#    fp_roc.close()
+
+    print "Draw balanced PR curve"
+    plt.figure(3)
+    plt.plot(recall, balanced_precision)
+
+    plt.xticks(np.arange(0, 1.1, 0.1))
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.title('Balanced-Precision vs. Recall Curve')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.grid(which='both', color='r', linestyle='--', linewidth=1)
+    plt.show()
+
+    plt.savefig(osp.join(save_dir, fname_balanced_pr_img))
+
+
+if __name__ == "__main__":
+
+    same_pairs_result_file = "samefacesim.txt"
+    diff_pairs_result_file = "diffacesim.txt"
+    save_dir = './'
+
+    roc_file = osp.join(save_dir, fname_roc)
+    if osp.exists(roc_file):
+        tp, fn, tn, fp = load_roc_txt(roc_file)
+    else:
+        tp, fn, tn, fp = calc_roc_and_pr(
+            same_pairs_result_file, diff_pairs_result_file, save_dir=save_dir)
+
+    draw_analysis_figure(tp, fn, tn, fp, save_dir=save_dir)
---
+++ face_verification_eval/eval_roc_and_pr.py
@@ -0,0 +1,129 @@
+"""Validate a face recognizer on the "Labeled Faces in the Wild" dataset (http://vis-www.cs.umass.edu/lfw/).
+Embeddings are pre-calculated and saved into a matlab .mat file.
+"""
+
+import numpy as np
+import argparse
+import sys
+import os
+import os.path as osp
+
+from load_features import load_mat_features
+from draw_pr_curve_zyf import calc_roc, calc_presicion_recall, draw_analysis_figure
+
+
+def load_image_pairs(pairs_file):
+    print "Load image pairs form file: ", pairs_file
+    fp = open(pairs_file, 'r')
+    idx_list = []
+    img_pair_list = []
+
+    for line in fp:
+        line_split = line.split()
+        idx_list.append((int(line_split[0]), int(line_split[1])))
+        img_pair_list.append((line_split[-1], line_split[-1]))
+
+    fp.close()
+
+    return (idx_list, img_pair_list)
+
+
+def calc_similarity(all_ftr_mat, pairs_idx_list, distance_type='cosine'):
+    print "Calc similarities of pairs"
+
+    sim_list = []
+    if distance_type == 'squared':
+        for idx_pair in pairs_idx_list:
+            dist_vec = all_ftr_mat[idx_pair[0]] - all_ftr_mat[idx_pair[1]]
+            sim = -np.dot(dist_vec, dist_vec.T)
+            sim_list.append(sim)
+    else:
+        for idx_pair in pairs_idx_list:
+            sim = np.dot(all_ftr_mat[idx_pair[0]], all_ftr_mat[idx_pair[1]])
+            sim_list.append(sim)
+
+    return sim_list
+
+
+def eval_roc_and_pr(argv):
+
+    args = parse_arguments(argv)
+
+    print 'args:', args
+#    for k,v in args:
+#        print "{}: {}".format(k,v)
+    save_dir = args.save_dir
+    if not osp.exists(save_dir):
+        os.makedirs(save_dir)
+
+    do_norm = not(args.no_normalize)
+    ftr_mat = load_mat_features(args.feature_mat_file, do_norm)
+
+    same_pairs_idx_list, same_img_list = load_image_pairs(args.same_pairs_file)
+    same_sim_list = calc_similarity(ftr_mat, same_pairs_idx_list)
+
+    fn_same = osp.join(save_dir, 'same_pairs_similarity.txt')
+    fp_same = open(fn_same, 'w')
+
+    for (i, sim) in enumerate(same_sim_list):
+        fp_same.write("%s %s %f\n" %
+                      (same_img_list[i][0], same_img_list[i][1], sim))
+    fp_same.close()
+
+    diff_pairs_idx_list, diff_img_list = load_image_pairs(args.diff_pairs_file)
+    diff_sim_list = calc_similarity(ftr_mat, diff_pairs_idx_list)
+
+    fn_diff = osp.join(save_dir, 'diff_pairs_similarity.txt')
+    fp_diff = open(fn_diff, 'w')
+
+    for (i, sim) in enumerate(diff_sim_list):
+        fp_diff.write("%s %s %f\n" %
+                      (diff_img_list[i][0], diff_img_list[i][1], sim))
+    fp_diff.close()
+
+    threshs = None
+    tp, fn, tn, fp = calc_roc(same_sim_list, diff_sim_list, threshs, save_dir)
+    calc_presicion_recall(tp, fn, tn, fp, threshs, save_dir)
+    draw_analysis_figure(tp, fn, tn, fp, save_dir)
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('feature_mat_file', type=str,
+                        help='Path to the .mat feature file.')
+    parser.add_argument('--same_pairs_file', type=str,
+                        help='The file of same pairs list',
+                        default='./test_pairs_same.txt')
+    parser.add_argument('--diff_pairs_file', type=str,
+                        help='The file of diff pairs list',
+                        default='./test_pairs_diff.txt')
+    parser.add_argument('--save_dir', type=str,
+                        help='directory for saving result files',
+                        default='./eval_rlt')
+    parser.add_argument('--distance', type=str,
+                        help='how to calc distance: <cosine or squared>.',
+                        default='cosine')
+    parser.add_argument('--no_normalize', action='store_true',
+                        help='do not normalize when loading features')
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        #        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/LFW-mtcnn-aligned-96x112_center_face_model_orig.mat'
+        ftr_path = 'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/LFW-mtcnn-aligned-96x112_0504_val0.15_iter_28000.mat'
+#        ftr_path = r'C:/zyf/dnn_models/face_models/centerloss/lfw_eval_results/LFW-mtcnn-simaligned-96x112_center_face_model_orig.mat'
+        same_pairs_file = './test_pairs_same.txt'
+        diff_pairs_file = './test_pairs_diff.txt'
+
+        save_dir = './eval_rlt'
+        distance = 'cosine'
+
+        sys.argv.append(ftr_path)
+        sys.argv.append('--same_pairs_file={}'.format(same_pairs_file))
+        sys.argv.append('--diff_pairs_file={}'.format(diff_pairs_file))
+        sys.argv.append('--save_dir={}'.format(save_dir))
+        sys.argv.append('--distance=' + distance)
+
+    eval_roc_and_pr(sys.argv[1:])
